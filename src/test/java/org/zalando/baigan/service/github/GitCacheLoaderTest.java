b5afd3a435fc7b4e5bb4454a3d82dff7b764eeb4
lmineiro <luis.mineiro@zalando.de>
2016-08-11 11:55:06 +0200

    Fixed the threading issue

--- src/test/java/org/zalando/baigan/service/github/GitCacheLoaderTest.java
+++ src/test/java/org/zalando/baigan/service/github/GitCacheLoaderTest.java
@@ -1,10 +1,7 @@
-package org.zalando.baigan.service;
+package org.zalando.baigan.service.github;
 
-import static org.junit.Assert.assertThat;
-
-import java.io.IOException;
-import java.util.Map;
-
+import com.google.common.collect.ImmutableList;
+import com.google.common.util.concurrent.ListenableFuture;
 import org.apache.cassandra.utils.MD5Digest;
 import org.apache.commons.codec.binary.Base64;
 import org.eclipse.egit.github.core.RepositoryContents;
@@ -12,14 +9,18 @@ import org.eclipse.egit.github.core.service.ContentsService;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.zalando.baigan.model.Configuration;
-import org.zalando.baigan.service.github.GitCacheLoader;
-import org.zalando.baigan.service.github.GitConfig;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.util.concurrent.ListenableFuture;
+import java.io.IOException;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GitCacheLoaderTest {
@@ -38,22 +39,20 @@ public class GitCacheLoaderTest {
 
         final GitConfig config = new GitConfig("somehost", "someowner",
                 "somerepo", "master", "somefile", "aoth_token");
-        ContentsService contentService = Mockito.mock(ContentsService.class);
+        final ContentsService contentService = mock(ContentsService.class);
 
         final GitCacheLoader loader = new GitCacheLoader(config,
                 contentService);
 
-        Mockito.when(
-                contentService.getContents(org.mockito.Matchers.anyObject(),
-                        org.mockito.Matchers.eq("staging.json"),
-                        org.mockito.Matchers.eq("master")))
+        when(contentService.getContents(anyObject(),
+                eq("staging.json"),
+                eq("master")))
                 .thenReturn(ImmutableList
                         .of(createRepositoryContents(testConfiguration1)));
 
-        Map<String, Configuration> configurations = loader.load("staging.json");
-        assertThat(configurations.size(), Matchers.equalTo(1));
-        assertThat(configurations.get("express.feature.toggle"),
-                Matchers.notNullValue());
+        final Map<String, Configuration> configurations = loader.load("staging.json");
+        assertThat(configurations.size(), equalTo(1));
+        assertThat(configurations.get("express.feature.toggle"), notNullValue());
 
     }
 
@@ -62,27 +61,24 @@ public class GitCacheLoaderTest {
 
         final GitConfig config = new GitConfig("somehost", "someowner",
                 "somerepo", "master", "somefile", "aoth_token");
-        ContentsService contentService = Mockito.mock(ContentsService.class);
+        final ContentsService contentService = mock(ContentsService.class);
 
         final GitCacheLoader loader = new GitCacheLoader(config,
                 contentService);
 
-        Mockito.when(
-                contentService.getContents(org.mockito.Matchers.anyObject(),
-                        org.mockito.Matchers.eq("staging.json"),
-                        org.mockito.Matchers.eq("master")))
+        when(contentService.getContents(anyObject(),
+                eq("staging.json"),
+                eq("master")))
                 .thenReturn(ImmutableList
                         .of(createRepositoryContents(testConfiguration1)));
 
         Map<String, Configuration> configurations = loader.load("staging.json");
-        assertThat(configurations.size(), Matchers.equalTo(1));
-        assertThat(configurations.get("express.feature.toggle"),
-                Matchers.notNullValue());
+        assertThat(configurations.size(), equalTo(1));
+        assertThat(configurations.get("express.feature.toggle"), notNullValue());
 
-        Mockito.when(
-                contentService.getContents(org.mockito.Matchers.anyObject(),
-                        org.mockito.Matchers.eq("staging.json"),
-                        org.mockito.Matchers.eq("master")))
+        when(contentService.getContents(anyObject(),
+                eq("staging.json"),
+                eq("master")))
                 .thenReturn(ImmutableList
                         .of(createRepositoryContents(testConfiguration2)));
 
@@ -93,13 +89,10 @@ public class GitCacheLoaderTest {
                 .get();
         assertThat(configurations2, Matchers.not(configurations));
 
-        assertThat(configurations2.size(), Matchers.equalTo(2));
-        assertThat(configurations.get("express.feature.toggle"),
-                Matchers.notNullValue());
+        assertThat(configurations2.size(), equalTo(2));
+        assertThat(configurations.get("express.feature.toggle"), notNullValue());
 
-        assertThat(configurations2.get("express.feature.serviceUrl"),
-                Matchers.notNullValue());
-
+        assertThat(configurations2.get("express.feature.serviceUrl"), notNullValue());
     }
 
     @Test
@@ -107,36 +100,30 @@ public class GitCacheLoaderTest {
 
         final GitConfig config = new GitConfig("somehost", "someowner",
                 "somerepo", "master", "somefile", "aoth_token");
-        ContentsService contentService = Mockito.mock(ContentsService.class);
+        final ContentsService contentService = mock(ContentsService.class);
 
-        final GitCacheLoader loader = new GitCacheLoader(config,
-                contentService);
+        final GitCacheLoader loader = new GitCacheLoader(config, contentService);
 
-        Mockito.when(
-                contentService.getContents(org.mockito.Matchers.anyObject(),
-                        org.mockito.Matchers.eq("staging.json"),
-                        org.mockito.Matchers.eq("master")))
+        when(contentService.getContents(anyObject(),
+                eq("staging.json"),
+                eq("master")))
                 .thenReturn(ImmutableList
                         .of(createRepositoryContents(testConfiguration2)));
 
-        final Map<String, Configuration> configurations1 = loader
-                .load("staging.json");
+        final Map<String, Configuration> configurations1 = loader.load("staging.json");
 
-        Mockito.when(
-                contentService.getContents(org.mockito.Matchers.anyObject(),
-                        org.mockito.Matchers.eq("staging.json"),
-                        org.mockito.Matchers.eq("master")))
+        when(contentService.getContents(anyObject(),
+                eq("staging.json"),
+                eq("master")))
                 .thenReturn(ImmutableList
                         .of(createRepositoryContents(testConfiguration2)));
 
         final ListenableFuture<Map<String, Configuration>> configurations2Future = loader
                 .reload("staging.json", configurations1);
 
-        final Map<String, Configuration> configurations2 = configurations2Future
-                .get();
+        final Map<String, Configuration> configurations2 = configurations2Future.get();
 
-        assertThat(configurations2, Matchers.equalTo(configurations1));
-
+        assertThat(configurations2, equalTo(configurations1));
     }
 
     @Test
@@ -144,42 +131,40 @@ public class GitCacheLoaderTest {
 
         final GitConfig config = new GitConfig("somehost", "someowner",
                 "somerepo", "master", "somefile", "aoth_token");
-        ContentsService contentService = Mockito.mock(ContentsService.class);
+        final ContentsService contentService = mock(ContentsService.class);
+        final GitCacheLoader loader = new GitCacheLoader(config, contentService);
 
-        final GitCacheLoader loader = new GitCacheLoader(config,
-                contentService);
-
-        Mockito.when(
-                contentService.getContents(org.mockito.Matchers.anyObject(),
-                        org.mockito.Matchers.eq("staging.json"),
-                        org.mockito.Matchers.eq("master")))
+        when(
+                contentService.getContents(anyObject(),
+                        eq("staging.json"),
+                        eq("master")))
                 .thenReturn(ImmutableList
                         .of(createRepositoryContents(testConfiguration1)));
 
 
         Map<String, Configuration> configurations = loader.load("staging.json");
-        assertThat(configurations.size(), Matchers.equalTo(1));
+        assertThat(configurations.size(), equalTo(1));
         assertThat(configurations.get("express.feature.toggle"),
-                Matchers.notNullValue());
+                notNullValue());
 
         //throw exception on retrieval
-        Mockito.doThrow(new IOException()).when(contentService).getContents(org.mockito.Matchers.anyObject(),
-                org.mockito.Matchers.eq("staging.json"),
-                org.mockito.Matchers.eq("master"));
+        doThrow(new IOException())
+                .when(contentService).getContents(anyObject(),
+                eq("staging.json"),
+                eq("master"));
 
         final ListenableFuture<Map<String, Configuration>> configurations2Future = loader
                 .reload("staging.json", configurations);
 
-        final Map<String, Configuration> configurations2 = configurations2Future
-                .get();
-        assertThat(configurations2, Matchers.equalTo(configurations));
+        final Map<String, Configuration> configurations2 = configurations2Future.get();
+        assertThat(configurations2, equalTo(configurations));
 
     }
 
     private RepositoryContents createRepositoryContents(final String text) {
         final RepositoryContents contents = new RepositoryContents();
-        byte[] base64OfCOnfig1 = Base64.encodeBase64(text.getBytes());
-        contents.setContent(new String(base64OfCOnfig1));
+        byte[] base64OfConfig1 = Base64.encodeBase64(text.getBytes());
+        contents.setContent(new String(base64OfConfig1));
         contents.setSha(MD5Digest.compute(text).toString());
         contents.setEncoding("base64");
 
