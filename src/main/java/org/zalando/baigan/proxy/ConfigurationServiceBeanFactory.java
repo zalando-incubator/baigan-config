22c041d30ea6e3ffcee4bb904473938001bd1e17
Lukas Niemeier <lukas.niemeier@zalando.de>
2017-05-11 14:13:35 +0200

    Explicit dependencies on configuration bean factory.

--- src/main/java/org/zalando/baigan/proxy/ConfigurationServiceBeanFactory.java
+++ src/main/java/org/zalando/baigan/proxy/ConfigurationServiceBeanFactory.java
@@ -1,15 +1,11 @@
 package org.zalando.baigan.proxy;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.AbstractFactoryBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.zalando.baigan.annotation.BaiganConfig;
-import org.zalando.baigan.proxy.handler.ConfigurationMethodInvocationHandler;
-import org.zalando.baigan.service.ConfigurationRepository;
-
 import com.google.common.base.Preconditions;
 import com.google.common.reflect.Reflection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AbstractFactoryBean;
+import org.zalando.baigan.annotation.BaiganConfig;
+import org.zalando.baigan.proxy.handler.ConfigurationMethodInvocationHandler;
 
 /**
  * Factory class that creates the proxy implementations for the interfaces
@@ -18,23 +14,20 @@ import com.google.common.reflect.Reflection;
  * @author mchand
  *
  */
-public class ConfigurationServiceBeanFactory extends AbstractFactoryBean<Object>
-        implements ApplicationContextAware {
+public class ConfigurationServiceBeanFactory extends AbstractFactoryBean<Object> {
 
     private Class<?> candidateInterface;
 
-    private ConfigurationRepository configurationRepository;
+    private final ConfigurationMethodInvocationHandler methodInvocationHandler;
 
-    private ApplicationContext applicationContext;
+    @Autowired
+    public ConfigurationServiceBeanFactory(final ConfigurationMethodInvocationHandler methodInvocationHandler) {
+        this.methodInvocationHandler = methodInvocationHandler;
+    }
 
     public void setCandidateInterface(final Class<?> candidateInterface) {
         this.candidateInterface = candidateInterface;
     }
-
-    /**
-     * Returns a proxy that implements the given interface.
-     *
-     */
 
     protected Object createInstance() {
 
@@ -44,29 +37,12 @@ public class ConfigurationServiceBeanFactory extends AbstractFactoryBean<Object>
                 "This BeanFactory could only create Beans for classes annotated with "
                         + BaiganConfig.class.getName());
 
-        final ConfigurationMethodInvocationHandler invocationHandler = applicationContext
-                .getBean(ConfigurationMethodInvocationHandler.class);
-        return Reflection.newProxy(candidateInterface, invocationHandler);
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext)
-            throws BeansException {
-        this.applicationContext = applicationContext;
-
+        return Reflection.newProxy(candidateInterface, methodInvocationHandler);
     }
 
     @Override
     public Class<?> getObjectType() {
         return this.candidateInterface;
-    }
-
-    public void setConfigurationRepository(final ConfigurationRepository configurationRepository) {
-        this.configurationRepository = configurationRepository;
-    }
-
-    public ConfigurationRepository getConfigurationRespository() {
-        return this.configurationRepository;
     }
 
 }
