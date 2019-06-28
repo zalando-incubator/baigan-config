a9e0995f4af5adf00dd83b33de87b10405cb7f6a
Lukas Niemeier <lukas.niemeier@zalando.de>
2019-06-28 13:41:11 +0200

    Update libraries

--- example/src/main/java/org/zalando/baigan/example/Application.java
+++ example/src/main/java/org/zalando/baigan/example/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class Application {
+public class Application {
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
